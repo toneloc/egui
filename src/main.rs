d3de82c63512be77aa6bf7bacb6f8074d87bc690
toneloc <@tonklaus>
2025-01-09 14:00:45 -0500

    extracted func

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,28 @@
-use std::{any::Any, str::FromStr};
-
 mod types;
 mod price_feeds;
+mod stable;
 
 use eframe::{egui, App, Frame};
-use egui::{Frame as EguiFrame, Margin, Stroke, Color32, RichText, TextureHandle, TextureOptions};
+use egui::{load::SizedTexture, Color32, Frame as EguiFrame, Margin, Stroke, TextureHandle, TextureOptions};
+use image::{GrayImage, Luma};
 use ldk_node::{
-    bitcoin::{secp256k1::PublicKey, Address, Network}, lightning::{
+    bitcoin::{address, secp256k1::PublicKey, Address, Network}, lightning::{
         ln::{msgs::SocketAddress, types::ChannelId},
         offers::offer::Offer,
     }, Builder, ChannelDetails, Node
 };
 use hex;
-use image::{GrayImage, Luma};
-use lightning::routing::gossip::NodeId;
 use qrcode::{Color, QrCode};
-use ureq::Agent;
-
-use std::time::{Instant, Duration};
-
+use std::{str::FromStr, thread::sleep, time::{Duration, Instant}};
 use types::{Bitcoin, StableChannel, USD};
-use price_feeds::{calculate_median_price, fetch_prices, set_price_feeds};
+use crate::stable::{check_stability,list_channels, close_channels_to_address, connect_to_lsp_and_entry_node};
 
-#[derive(Clone)]
-struct UserData {
+struct MyApp {
     is_onboarding: bool,
     has_paid_initial_invoice: bool,
     waiting_for_invoice_payment: bool,
     public_key: u64,
-}
-
-impl Default for UserData {
-    fn default() -> Self {
-        Self {
-            is_onboarding: true,
-            has_paid_initial_invoice: false,
-            waiting_for_invoice_payment: false,
-            public_key: 0x123,
-        }
-    }
-}
-
-struct MyApp {
     last_stability_check: Instant,
-    user_data: UserData,
     invoice_result: String,
     user: Node,
     qr_texture: Option<TextureHandle>,
@@ -87,7 +66,6 @@ fn make_node(alias: &str, port: u16, lsp_pubkey: Option<PublicKey>) -> Node {
     } else {
         println!("No listening addresses found.");
     }
-
 
     node
 }
@@ -142,8 +120,11 @@ impl MyApp {
 
 
         Self {
+            is_onboarding: true,
+            has_paid_initial_invoice: false,
+            waiting_for_invoice_payment: false,
+            public_key: 0,
             last_stability_check: Instant::now() - Duration::from_secs(60),
-            user_data: UserData::default(),
             invoice_result: String::new(),
             user,
             qr_texture: None,
@@ -154,193 +135,25 @@ impl MyApp {
             showing_channels: false,
             close_channel_address: String::new(),
             network: Network::Signet,
-            frame
-        }
-    }
-
-    /// Core stability logic
-    fn check_stability(node: &Node, sc: &mut StableChannel) {
-        sc.latest_price = fetch_prices(&Agent::new(), &set_price_feeds())
-        .and_then(|prices| calculate_median_price(prices))
-        .unwrap_or(0.0);
-
-        if let Some(channel) = node
-            .list_channels()
-            .iter()
-            .find(|c| c.channel_id == sc.channel_id)
-        {
-            Self::update_balances(sc, Some(channel.clone()));
-        }
-
-        let mut dollars_from_par: USD = sc.stable_receiver_usd - sc.expected_usd;
-        let mut percent_from_par = ((dollars_from_par / sc.expected_usd) * 100.0).abs();
-
-        println!("{:<25} {:>15}", "Expected USD:", sc.expected_usd);
-        println!("{:<25} {:>15}", "User USD:", sc.stable_receiver_usd);
-        println!("{:<25} {:>5}", "Percent from par:", format!("{:.2}%\n", percent_from_par));
-
-        println!("{:<25} {:>15}", "User BTC:", sc.stable_receiver_btc);
-        println!("{:<25} {:>15}", "LSP USD:", sc.stable_provider_usd);
-
-        enum Action {
-            Wait,
-            Pay,
-            DoNothing,
-            HighRisk,
-        }
-
-        let action = if percent_from_par < 0.1 {
-            Action::DoNothing
-        } else {
-            let is_receiver_below_expected: bool = sc.stable_receiver_usd < sc.expected_usd;
-
-            match (sc.is_stable_receiver, is_receiver_below_expected, sc.risk_level > 100) {
-                (_, _, true) => Action::HighRisk, // High risk scenario
-                (true, true, false) => Action::Wait,   // We are User and below peg, wait for payment
-                (true, false, false) => Action::Pay,   // We are User and above peg, need to pay
-                (false, true, false) => Action::Pay,   // We are LSP and below peg, need to pay
-                (false, false, false) => Action::Wait, // We are LSP and above peg, wait for payment
-            }
-        };
-
-        match action {
-            Action::DoNothing => println!("\nDifference from par less than 0.1%. Doing nothing."),
-            Action::Wait => {
-                println!("\nWaiting 10 seconds and checking on payment...\n");
-                std::thread::sleep(std::time::Duration::from_secs(10));
-
-                if let Some(channel) = node
-                    .list_channels()
-                    .iter()
-                    .find(|c| c.channel_id == sc.channel_id)
-                {
-                    Self::update_balances(sc, Some(channel.clone()));
-                }
-
-                println!("{:<25} {:>15}", "Expected USD:", sc.expected_usd);
-                println!("{:<25} {:>15}", "User USD:", sc.stable_receiver_usd);
-
-                dollars_from_par = sc.stable_receiver_usd - sc.expected_usd;
-                percent_from_par = ((dollars_from_par / sc.expected_usd) * 100.0).abs();
-
-                println!(
-                    "{:<25} {:>5}",
-                    "Percent from par:",
-                    format!("{:.2}%\n", percent_from_par)
-                );
-
-                println!("{:<25} {:>15}", "LSP USD:", sc.stable_provider_usd);
-            }
-            Action::Pay => {
-                println!("\nPaying the difference...\n");
-
-                let amt = USD::to_msats(dollars_from_par, sc.latest_price);
-
-                // let result = node.bolt12_payment().send_using_amount(
-                //     &sc.counterparty_offer,
-                //     amt,
-                //     None,
-                //     Some("here ya go".to_string()),
-                // );
-
-                // This is keysend / spontaneous payment code you can use if Bolt12 doesn't work
-
-                // First, ensure we are connected
-                // let result = node.connect(sc.counterparty, sc.counterparty_net_address, true);
-
-                // if let Err(e) = result {
-                //     println!("Failed to connect with : {}", e);
-                // } else {
-                //     println!("Successfully connected.");
-                // }
-
-                let result = node
-                    .spontaneous_payment()
-                    .send(amt, sc.counterparty,None);
-                match result {
-                    Ok(payment_id) => println!("Payment sent successfully with payment ID: {}", payment_id),
-                    Err(e) => println!("Failed to send payment: {}", e),
-                }
-
-            }
-            Action::HighRisk => {
-                println!("Risk level high. Current risk level: {}", sc.risk_level);
-            }
-        }
-    }
-    
-    fn update_balances(sc: &mut StableChannel, channel_details: Option<ChannelDetails>) {
-
-        let (our_balance, their_balance) = match channel_details {
-            Some(channel) => {
-                let unspendable_punishment_sats = channel.unspendable_punishment_reserve.unwrap_or(0);
-                let our_balance_sats =
-                    (channel.outbound_capacity_msat / 1000) + unspendable_punishment_sats;
-                let their_balance_sats = channel.channel_value_sats - our_balance_sats;
-                (our_balance_sats, their_balance_sats)
-            }
-            None => (0, 0),
-        };
-
-        if sc.is_stable_receiver {
-            sc.stable_receiver_btc = Bitcoin::from_sats(our_balance);
-            sc.stable_receiver_usd = USD::from_bitcoin(sc.stable_receiver_btc, sc.latest_price);
-            sc.stable_provider_btc = Bitcoin::from_sats(their_balance);
-            sc.stable_provider_usd = USD::from_bitcoin(sc.stable_provider_btc, sc.latest_price);
-        } else {
-            sc.stable_provider_btc = Bitcoin::from_sats(our_balance);
-            sc.stable_provider_usd = USD::from_bitcoin(sc.stable_provider_btc, sc.latest_price);
-            sc.stable_receiver_btc = Bitcoin::from_sats(their_balance);
-            sc.stable_receiver_usd = USD::from_bitcoin(sc.stable_receiver_btc, sc.latest_price);
+            frame,
         }
     }
 
     fn get_jit_invoice(&mut self, ctx: &egui::Context) {
-        let _connected_to_lsp = self.user.connect(
-            PublicKey::from_str("025d4c41316f9d847ed3ec827751f1df4efabb6aa48c162b29f9aabf5eb148f8b1")
-                .unwrap(),
-            SocketAddress::from_str("127.0.0.1:9737").unwrap(),
-            true,
-        );
-
-        println!("Connection result: {:?}", _connected_to_lsp.unwrap());
-
-        let _connected_to_exchange = self.user.connect(
+        let _connected = self.user.connect(
             PublicKey::from_str("02e897f0ce1bf88afe1f8e2be0045294ec87b00eebd689e42ba7290cfa2922dbe7")
                 .unwrap(),
             SocketAddress::from_str("127.0.0.1:9735").unwrap(),
             true,
         );
-
-        println!("Connection result: {:?}", _connected_to_exchange.unwrap());
-
-        let node_info = self
-        .user
-        .network_graph()
-        .node(&NodeId::from_pubkey(
-            &PublicKey::from_str("025d4c41316f9d847ed3ec827751f1df4efabb6aa48c162b29f9aabf5eb148f8b1")
-                .unwrap(),
-        ));
-
-        println!("Node information: {:?}", node_info);
-
-        let node_info = self
-            .user
-            .network_graph()
-            .node(&NodeId::from_pubkey(
-                &PublicKey::from_str("02e897f0ce1bf88afe1f8e2be0045294ec87b00eebd689e42ba7290cfa2922dbe7")
-                    .unwrap(),
-            ));
-
-        println!("Node information: {:?}", node_info);
-            
+    
         let result = self.user.bolt11_payment().receive_via_jit_channel(
             30_000_000,
             "Stable Channel",
             3600,
             Some(10_000_000),
         );
-
+    
         match result {
             Ok(invoice) => {
                 self.invoice_result = invoice.to_string();
@@ -350,7 +163,7 @@ impl MyApp {
                 let scale_factor = 4;
                 let mut imgbuf =
                     GrayImage::new((width * scale_factor) as u32, (width * scale_factor) as u32);
-
+    
                 for y in 0..width {
                     for x in 0..width {
                         let color = if bits[y * width + x] == Color::Dark {
@@ -387,49 +200,7 @@ impl MyApp {
             }
         }
     }
-
-    fn list_channels(&mut self) {
-        let channels = self.user.list_channels();
-        if channels.is_empty() {
-            self.channel_list_string = "No channels found.".to_string();
-        } else {
-            let mut info = String::new();
-            info.push_str("User Channels:\n");
-            self.stable_channel.channel_id = channels[0].channel_id;
-            for channel in channels.iter() {
-                info.push_str("--------------------------------------------\n");
-                info.push_str(&format!("Channel ID: {}\n", channel.channel_id));
-                info.push_str(&format!("Channel Value: {} sats\n", channel.channel_value_sats));
-                info.push_str(&format!("Channel Ready?: {}\n", channel.is_channel_ready));
-            }
-            info.push_str("--------------------------------------------\n");
-            self.channel_list_string = info;
-        }
-        self.channel_list = channels;
-    }
-
-    fn close_channels_to_address(&mut self) {
-        for channel in self.user.list_channels().iter() {
-            let user_channel_id = channel.user_channel_id;
-            let counterparty_node_id = channel.counterparty_node_id;
-            let _ = self.user.close_channel(&user_channel_id, counterparty_node_id);
-        }
-
-        // Withdraw everything to address
-        let address_str = &self.close_channel_address;
-        match Address::from_str(address_str) {
-            Ok(addr) => match addr.require_network(self.network) {
-                Ok(addr_checked) => {
-                    match self.user.onchain_payment().send_all_to_address(&addr_checked) {
-                        Ok(txid) => println!("{}", txid),
-                        Err(e) => eprintln!("Error: {}", e),
-                    }
-                }
-                Err(_) => eprintln!("Invalid address for this network"),
-            },
-            Err(_) => eprintln!("Invalid address"),
-        }
-    }
+    
 }
 
 impl App for MyApp {
@@ -439,28 +210,25 @@ impl App for MyApp {
                 self.frame.show(ui, |ui| {
                     ui.heading("Stable Channels ⚖️💵⚡");
                     ui.add_space(80.0);
-
-                    
-
+                
                 let bigger_channel_button = egui::Button::new("Create a $100 stable channel")
                         .min_size(egui::vec2(180.0, 50.0));
 
-                if self.user_data.is_onboarding {
-                    self.list_channels();
+                if self.is_onboarding {
+                    list_channels(&mut self.user);
                     if !self.channel_list.is_empty() {
-                        self.user_data.waiting_for_invoice_payment = false;
-                        self.user_data.is_onboarding = false;
+                        self.waiting_for_invoice_payment = false;
+                        self.is_onboarding = false;
                     }
 
-                    if !self.user_data.waiting_for_invoice_payment && !self.user_data.has_paid_initial_invoice {
-                        
+                    if !self.waiting_for_invoice_payment && !self.has_paid_initial_invoice {
                         if ui.add(bigger_channel_button).clicked() {
+                            connect_to_lsp_and_entry_node(&self.user);
                             self.get_jit_invoice(ctx);
-                            self.user_data.waiting_for_invoice_payment = true;
-                        }
-                        
+                            self.waiting_for_invoice_payment = true;
+                    }
                 
-                    } else if self.user_data.waiting_for_invoice_payment {
+                    } else if self.waiting_for_invoice_payment {
                         if let Some(ref qr) = self.qr_texture {
                             ui.image(qr);
                         } else {
@@ -480,27 +248,29 @@ impl App for MyApp {
                             });
                         }
 
-                        if ui.button("Check Channels").clicked() {
-                            self.list_channels();
-                            if !self.channel_list.is_empty() {
-                                self.user_data.waiting_for_invoice_payment = false;
-                                self.user_data.is_onboarding = false;
-                                println!("{}", self.channel_list[0].channel_id);
-                                
-                            }
+                        let (channels, info) = list_channels(&self.user);
+                        self.channel_list = channels;
+                        self.channel_list_string = info;
+                                                
+                        if !self.channel_list.is_empty() {
+                            self.waiting_for_invoice_payment = false;
+                            self.is_onboarding = false;
+                            println!("{}", self.channel_list[0].channel_id);
+                            
                         }
+                        
                         ui.label(&self.channel_list_string);
 
                         if ui.button("Back").clicked() {
-                            self.user_data.waiting_for_invoice_payment = false;
-                            self.user_data.is_onboarding = true;
-                            self.user_data.waiting_for_invoice_payment = true;
+                            self.waiting_for_invoice_payment = false;
+                            self.is_onboarding = true;
+                            self.waiting_for_invoice_payment = true;
                         }
                     }
                 } else { // Regularly scheduled programming
                     let now = Instant::now();
                     if now.duration_since(self.last_stability_check) >= Duration::from_secs(30) {
-                        Self::check_stability(&self.user, &mut self.stable_channel);
+                        check_stability(&self.user, &mut self.stable_channel);
                         self.last_stability_check = now;
                     }
 
@@ -511,18 +281,18 @@ impl App for MyApp {
                     ui.label(format!("On-Chain Balance: {}", onchain_balance));
                     ui.label(format!("Lightning Balance: {}", lightning_balance));
                     if ui.button("List Channels").clicked() {
-                        self.list_channels();
+                        list_channels(&self.user);
                     }
                     ui.label(&self.channel_list_string);
 
                     // Address entry + close channels button
                     ui.text_edit_singleline(&mut self.close_channel_address);
                     if ui.button("Close channel to address").clicked() {
-                        self.close_channels_to_address();
+                        close_channels_to_address(&self.user, self.close_channel_address.clone());
                     }
 
                     if ui.button("Back").clicked() {
-                        self.user_data.is_onboarding = true;
+                        self.is_onboarding = true;
                     }
 
                 }
---
+++ src/stable.rs
@@ -0,0 +1,232 @@
+use std::str::FromStr;
+
+use egui::{TextureHandle, TextureOptions};
+use image::{GrayImage, Luma};
+use ldk_node::bitcoin::secp256k1::PublicKey;
+use ldk_node::bitcoin::{Address, Network};
+use ldk_node::{Node, ChannelDetails};
+use lightning::ln::msgs::SocketAddress;
+use lightning::ln::types::ChannelId;
+use lightning::routing::gossip::NodeId;
+use qrcode::{Color, QrCode};
+use ureq::Agent;
+use crate::types::{Bitcoin, StableChannel, USD};
+use crate::price_feeds::{calculate_median_price, fetch_prices, set_price_feeds};
+
+/// Core stability logic
+pub fn check_stability(node: &Node, sc: &mut StableChannel) {
+    sc.latest_price = fetch_prices(&Agent::new(), &set_price_feeds())
+    .and_then(|prices| calculate_median_price(prices))
+    .unwrap_or(0.0);
+
+    if let Some(channel) = node
+        .list_channels()
+        .iter()
+        .find(|c| c.channel_id == sc.channel_id)
+    {
+        update_balances(sc, Some(channel.clone()));
+    }
+
+    let mut dollars_from_par: USD = sc.stable_receiver_usd - sc.expected_usd;
+    let mut percent_from_par = ((dollars_from_par / sc.expected_usd) * 100.0).abs();
+
+    println!("{:<25} {:>15}", "Expected USD:", sc.expected_usd);
+    println!("{:<25} {:>15}", "User USD:", sc.stable_receiver_usd);
+    println!("{:<25} {:>5}", "Percent from par:", format!("{:.2}%\n", percent_from_par));
+
+    println!("{:<25} {:>15}", "User BTC:", sc.stable_receiver_btc);
+    println!("{:<25} {:>15}", "LSP USD:", sc.stable_provider_usd);
+
+    enum Action {
+        Wait,
+        Pay,
+        DoNothing,
+        HighRisk,
+    }
+
+    let action = if percent_from_par < 0.1 {
+        Action::DoNothing
+    } else {
+        let is_receiver_below_expected: bool = sc.stable_receiver_usd < sc.expected_usd;
+
+        match (sc.is_stable_receiver, is_receiver_below_expected, sc.risk_level > 100) {
+            (_, _, true) => Action::HighRisk, // High risk scenario
+            (true, true, false) => Action::Wait,   // We are User and below peg, wait for payment
+            (true, false, false) => Action::Pay,   // We are User and above peg, need to pay
+            (false, true, false) => Action::Pay,   // We are LSP and below peg, need to pay
+            (false, false, false) => Action::Wait, // We are LSP and above peg, wait for payment
+        }
+    };
+
+    match action {
+        Action::DoNothing => println!("\nDifference from par less than 0.1%. Doing nothing."),
+        Action::Wait => {
+            println!("\nWaiting 10 seconds and checking on payment...\n");
+            std::thread::sleep(std::time::Duration::from_secs(10));
+
+            if let Some(channel) = node
+                .list_channels()
+                .iter()
+                .find(|c| c.channel_id == sc.channel_id)
+            {
+                update_balances(sc, Some(channel.clone()));
+            }
+
+            println!("{:<25} {:>15}", "Expected USD:", sc.expected_usd);
+            println!("{:<25} {:>15}", "User USD:", sc.stable_receiver_usd);
+
+            dollars_from_par = sc.stable_receiver_usd - sc.expected_usd;
+            percent_from_par = ((dollars_from_par / sc.expected_usd) * 100.0).abs();
+
+            println!(
+                "{:<25} {:>5}",
+                "Percent from par:",
+                format!("{:.2}%\n", percent_from_par)
+            );
+
+            println!("{:<25} {:>15}", "LSP USD:", sc.stable_provider_usd);
+        }
+        Action::Pay => {
+            println!("\nPaying the difference...\n");
+
+            let amt = USD::to_msats(dollars_from_par, sc.latest_price);
+
+            // let result = node.bolt12_payment().send_using_amount(
+            //     &sc.counterparty_offer,
+            //     amt,
+            //     None,
+            //     Some("here ya go".to_string()),
+            // );
+
+            // This is keysend / spontaneous payment code you can use if Bolt12 doesn't work
+
+            // First, ensure we are connected
+            // let result = node.connect(sc.counterparty, sc.counterparty_net_address, true);
+
+            // if let Err(e) = result {
+            //     println!("Failed to connect with : {}", e);
+            // } else {
+            //     println!("Successfully connected.");
+            // }
+
+            let result = node
+                .spontaneous_payment()
+                .send(amt, sc.counterparty,None);
+            match result {
+                Ok(payment_id) => println!("Payment sent successfully with payment ID: {}", payment_id),
+                Err(e) => println!("Failed to send payment: {}", e),
+            }
+
+        }
+        Action::HighRisk => {
+            println!("Risk level high. Current risk level: {}", sc.risk_level);
+        }
+    }
+}
+
+pub fn update_balances(sc: &mut StableChannel, channel_details: Option<ChannelDetails>) {
+
+    let (our_balance, their_balance) = match channel_details {
+        Some(channel) => {
+            let unspendable_punishment_sats = channel.unspendable_punishment_reserve.unwrap_or(0);
+            let our_balance_sats =
+                (channel.outbound_capacity_msat / 1000) + unspendable_punishment_sats;
+            let their_balance_sats = channel.channel_value_sats - our_balance_sats;
+            (our_balance_sats, their_balance_sats)
+        }
+        None => (0, 0),
+    };
+
+    if sc.is_stable_receiver {
+        sc.stable_receiver_btc = Bitcoin::from_sats(our_balance);
+        sc.stable_receiver_usd = USD::from_bitcoin(sc.stable_receiver_btc, sc.latest_price);
+        sc.stable_provider_btc = Bitcoin::from_sats(their_balance);
+        sc.stable_provider_usd = USD::from_bitcoin(sc.stable_provider_btc, sc.latest_price);
+    } else {
+        sc.stable_provider_btc = Bitcoin::from_sats(our_balance);
+        sc.stable_provider_usd = USD::from_bitcoin(sc.stable_provider_btc, sc.latest_price);
+        sc.stable_receiver_btc = Bitcoin::from_sats(their_balance);
+        sc.stable_receiver_usd = USD::from_bitcoin(sc.stable_receiver_btc, sc.latest_price);
+    }
+}
+
+pub fn connect_to_lsp_and_entry_node(node: &Node) {
+    let _connected_to_lsp = node.connect(
+        PublicKey::from_str("025d4c41316f9d847ed3ec827751f1df4efabb6aa48c162b29f9aabf5eb148f8b1")
+            .unwrap(),
+        SocketAddress::from_str("127.0.0.1:9737").unwrap(),
+        true,
+    );
+
+    println!("Connection result: {:?}", _connected_to_lsp.unwrap());
+
+    let _connected_to_exchange = node.connect(
+        PublicKey::from_str("02e897f0ce1bf88afe1f8e2be0045294ec87b00eebd689e42ba7290cfa2922dbe7")
+            .unwrap(),
+        SocketAddress::from_str("127.0.0.1:9735").unwrap(),
+        true,
+    );
+
+    println!("Connection result: {:?}", _connected_to_exchange.unwrap());
+
+    let node_info = node
+        .network_graph()
+        .node(&NodeId::from_pubkey(
+            &PublicKey::from_str("025d4c41316f9d847ed3ec827751f1df4efabb6aa48c162b29f9aabf5eb148f8b1")
+                .unwrap(),
+        ));
+
+    println!("Node information: {:?}", node_info);
+
+    let node_info = node
+        .network_graph()
+        .node(&NodeId::from_pubkey(
+            &PublicKey::from_str("02e897f0ce1bf88afe1f8e2be0045294ec87b00eebd689e42ba7290cfa2922dbe7")
+                .unwrap(),
+        ));
+
+    println!("Node information: {:?}", node_info);
+}
+
+pub fn list_channels(node: &Node) -> (Vec<ChannelDetails>, String) {
+    let channels = node.list_channels();
+    let mut info = String::new();
+
+    if channels.is_empty() {
+        info.push_str("No channels found.");
+    } else {
+        info.push_str("User Channels:\n");
+        for channel in &channels {
+            info.push_str("--------------------------------------------\n");
+            info.push_str(&format!("Channel ID: {}\n", channel.channel_id));
+            info.push_str(&format!("Channel Value: {} sats\n", channel.channel_value_sats));
+            info.push_str(&format!("Channel Ready?: {}\n", channel.is_channel_ready));
+        }
+        info.push_str("--------------------------------------------\n");
+    }
+
+    (channels, info)
+}
+
+pub fn close_channels_to_address(node: &Node, address_str: String) {
+    for channel in node.list_channels().iter() {
+        let user_channel_id = channel.user_channel_id;
+        let counterparty_node_id = channel.counterparty_node_id;
+        let _ = node.close_channel(&user_channel_id, counterparty_node_id);
+    }
+
+    // Withdraw everything to address
+    match Address::from_str(&address_str) {
+
+        Ok(addr) => match addr.require_network(Network::Signet,) {
+            Ok(addr_checked) => {
+                match node.onchain_payment().send_all_to_address(&addr_checked) {
+                    Ok(txid) => println!("{}", txid),
+                    Err(e) => eprintln!("Error: {}", e),
+                }
+            }
+            Err(_) => eprintln!("Invalid address for this network"),
+        },
+        Err(_) => eprintln!("Invalid address"),
+    }
+}
